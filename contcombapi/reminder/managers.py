8607dfd34ce01b1b4de6f9c988079af9ca5ffdd4
Bruno Zeraik <brunozrk@gmail.com>
2014-05-07 20:25:48 -0300

    Module Reminder

---
+++ contcombapi/reminder/managers.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+'''
+@author:: Bruno Zeraik
+'''
+from contcombapi.manager.BaseManager import BaseManager
+
+class ReminderManager(BaseManager):
+    '''
+        Base class for managing the operations to database
+    '''
